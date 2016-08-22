144efa7ca67d1b53c60b11247d8ed3089a14b4c9
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2016-08-21 23:33:05 -0400

    Refactor to reusable methods

--- main.py
+++ main.py
@@ -15,39 +15,49 @@ def main():
     assert verifiedOutcome is not None, \
         'Outcome {} was not found'.format(outcomeID)
 
-    matchingCourseIDs = set()  # Used for further processing
+    courseIDs = config.Canvas.COURSE_ID_SET
+    print 'possible course IDs:', courseIDs
 
-    print 'possible course IDs:', config.Canvas.COURSE_ID_SET
-
-    for courseID in config.Canvas.COURSE_ID_SET:
-        courseOutcomeGroupLinks = canvas.getCoursesOutcomeGroupLinksObjects(courseID)
-
-        # Is it possible to short-circuit this using itertools?
-        matchingCourseIDs.update(
-            {courseID for outcomeLink in courseOutcomeGroupLinks
-             if outcomeLink.outcome.id == verifiedOutcome.id}
-        )
+    matchingCourseIDs = getCoursesWithOutcome(canvas, courseIDs, verifiedOutcome)
 
     print 'matching course IDs:', matchingCourseIDs
 
     assert len(matchingCourseIDs) > 0, \
         'No courses linked to Outcome {} were found'.format(outcomeID)
 
-    matchingCourseAssignments = []
-
-    for courseID in matchingCourseIDs:
-        courseAssignments = canvas.getCoursesAssignmentsObjects(courseID)
-
-        for assignment in courseAssignments:
-            for rubric in assignment.rubric:
-                if rubric.outcome_id == verifiedOutcome.id:
-                    matchingCourseAssignments.append(assignment)
-                    break
+    matchingCourseAssignments = getCourseAssignmentsWithOutcome(canvas, matchingCourseIDs, verifiedOutcome)
 
     print 'matching course assignments:', [
         (assignment.name, assignment.id)
         for assignment
         in matchingCourseAssignments]
+
+
+def getCoursesWithOutcome(canvas, courseIDs, outcome):
+    matchingCourseIDs = set()
+    for courseID in courseIDs:
+        courseOutcomeGroupLinks = canvas.getCoursesOutcomeGroupLinksObjects(courseID)
+
+        # Is it possible to short-circuit this using itertools?
+        matchingCourseIDs.update(
+            {courseID for outcomeLink in courseOutcomeGroupLinks
+             if outcomeLink.outcome.id == outcome.id}
+        )
+    return matchingCourseIDs
+
+
+def getCourseAssignmentsWithOutcome(canvas, courseIDs, outcome):
+    matchingCourseAssignments = []
+    for courseID in courseIDs:
+        courseAssignments = canvas.getCoursesAssignmentsObjects(courseID)
+
+        for assignment in courseAssignments:
+            for rubric in assignment.rubric:
+                if rubric.outcome_id == outcome.id:
+                    matchingCourseAssignments.append(assignment)
+                    break
+    return matchingCourseAssignments
+
 
 if '__main__' == __name__:
     main()
