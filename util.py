a243505ee7392c9fdde46a1a8d0a00e6691375bd
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2016-08-31 17:12:16 -0400

    arcgis-api: Added "CaptureStdoutLines" context manager to capture lines *PRINTED* by the ArcREST module.  (Seriously‽)

--- util.py
+++ util.py
@@ -1,9 +1,38 @@
 import inspect
+import sys
+from cStringIO import StringIO
 
 
 class UtilMixin(object):
     def methodName(self, depth=1):
         return inspect.currentframe(depth).f_code.co_name
+
+
+class CaptureStdoutLines(list):
+    """
+    A context manager for capturing the lines sent to stdout as elements
+    of a list.  Useful for capturing important output printed by
+    poorly-designed API methods.
+
+    Example::
+        with CaptureStdoutLines() as output:
+            print('Norwegian blue parrot')
+        assert output == ['Norwegian blue parrot']
+
+        with CaptureStdoutLines(output) as output:
+            print('Venezuelan beaver cheese')
+        assert output == ['Norwegian blue parrot', 'Venezuelan beaver cheese']
+    """
+
+    def __enter__(self):
+        self._originalStdout = sys.stdout
+        sys.stdout = self._stdoutStream = StringIO()
+        return self
+
+    def __exit__(self, *args):
+        self.extend(self._stdoutStream.getvalue().splitlines())
+        self.notEmpty = len(self) != 0
+        sys.stdout = self._originalStdout
 
 
 def stringContainsAllCharacters(string, characters):
