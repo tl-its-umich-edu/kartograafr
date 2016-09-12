bb59074839f3851eb124f1cf21ead644e503b29f
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2016-09-11 21:40:12 -0400

    logging-email - Simplified name and ID formatter, which will only be used with ArcGIS objects.  Added logging formatter to fix timestamps.  Added context manager to allow logging level and handlers to be modified.

--- util.py
+++ util.py
@@ -1,4 +1,6 @@
+import datetime
 import inspect
+import logging
 import sys
 from cStringIO import StringIO
 
@@ -51,9 +53,101 @@ def stringContainsAllCharacters(string, characters):
 
 
 def formatNameAndID(object):
-    name = object.name if 'name' in object else object.title
-    return '"{}" ({})'.format(name, object.id)
+    return '"{}" ({})'.format(object.title, object.id)
 
 
-def formatNamesAndIDs(objects):
-    return map(formatNameAndID, objects)
+class Iso8601UTCTimeFormatter(logging.Formatter):
+    """
+    A logging Formatter class giving timestamps in a more common ISO 8601 format.
+
+    The default logging.Formatter class **claims** to give timestamps in ISO 8601 format
+    if it is not initialized with a different timestamp format string.  However, its
+    format, "YYYY-MM-DD hh:mm:ss,sss", is much less common than, "YYYY-MM-DDThh:mm:ss.sss".
+    That is, the separator between date and time is a space instead of the letter "T"
+    and the separator for fractional seconds is a comma instead of a period (full stop).
+    While these differences may not be strictly *wrong*, it makes the formatted timestamp
+    *unusual*.
+
+    This formatter class removes some of the differences by using Python's own
+    datetime.datetime.isoformat() method.  That method uses "T" as the default separator
+    between date and time.  And it always uses a period (full stop) for fractional
+    seconds, even if a comma is normally used for fractional numbers in the current
+    locale.
+
+    This class also assumes the timestamp should use the UTC timezone.
+    """
+
+    def __init__(self, logFormat=None, timeFormat=None):
+        """
+        The purpose of this constructor is to set the timezone to UTC for later use.
+
+        :param logFormat: Log record formatting string.
+        :type logFormat: str
+        :param timeFormat: Time formatting string. You probably **DO NOT** want one.
+        :type timeFormat: str
+        """
+        super(Iso8601UTCTimeFormatter, self).__init__(logFormat, timeFormat)
+
+        from dateutil.tz import tzutc
+        self._TIMEZONE_UTC = tzutc()
+
+    def formatTime(self, record, timeFormat=None):
+        """
+        In the event that a timeFormat string is given, this method will use the
+        formatTime() method of the superclass (logging.Formatter) instead.  That's
+        because this method doesn't need timeFormat. So, other than this solution,
+        the options for handling that argument were to ignore it or raise an exception,
+        either of which probably violate the principle of least astonishment (POLA).
+
+        :param record: Record of the current log entry
+        :type record: logging.LogRecord
+        :param timeFormat: Time formatting string. You probably **DO NOT** want one.
+        :type timeFormat: str
+        :return: Log record's timestamp in ISO 8601 format
+        :rtype: str or unicode
+        """
+        if timeFormat is not None:
+            return super(Iso8601UTCTimeFormatter, self).formatTime(record, timeFormat)
+
+        return datetime.datetime.fromtimestamp(record.created, self._TIMEZONE_UTC).isoformat()
+
+
+class LoggingContext(object):
+    """
+    A context manager to temporarily change the log level or add a handler
+    to logging statements for a block of code.
+
+    Example::
+        # Assuming logger doesn't log to console, add a handler to log to console
+        with LoggingContext(logger, handler=logging.StreamHandler()):
+            logger.info('Logged to console')
+
+        logger.info('No longer logging to console')
+
+        # Assuming logger works at INFO level or lower, change the level to log only ERROR or higher
+        with LoggingContext(logger, level=logging.ERROR):
+            logger.info('Not logged because INFO < ERROR')
+
+        logger.info('Logged because INFO is allowed again')
+    """
+
+    def __init__(self, logger, level=None, handler=None, close=False):
+        self.logger = logger
+        self.level = level
+        self.handler = handler
+        self.close = close
+
+    def __enter__(self):
+        if self.level is not None:
+            self.oldLevel = self.logger.level
+            self.logger.setLevel(self.level)
+        if self.handler:
+            self.logger.addHandler(self.handler)
+
+    def __exit__(self, exceptionType, exceptionValue, traceback):
+        if self.level is not None:
+            self.logger.setLevel(self.oldLevel)
+        if self.handler:
+            self.logger.removeHandler(self.handler)
+        if self.handler and self.close:
+            self.handler.close()
