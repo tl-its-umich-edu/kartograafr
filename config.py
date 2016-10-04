3be06939b6dd21a7ffc7691418c8ffd6ea55da24
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2016-10-04 16:55:12 -0400

    Changed/added email configuration parameters, added logger name, added note about usage of COURSE_ID_SET.

--- config.py
+++ config.py
@@ -4,10 +4,13 @@ import os
 class Application(object):
     class Email(object):
         SMTP_SERVER = '127.0.0.1'
-        FROM_ADDRESS = '"ArcGIS-Canvas Service" <kartograafr-service@umich.edu>'
+        SENDER_ADDRESS = '"ArcGIS-Canvas Service" <kartograafr-service@umich.edu>'
+        RECIPIENT_AT_DOMAIN = '@umich.edu'
         SUBJECT = 'ArcGIS-Canvas logs for course ID {courseID}'
 
+
     class Logging(object):
+        MAIN_LOGGER_NAME = 'kartograafr'
         DIRECTORY = '/tmp/log'
         COURSE_DIRECTORY = os.path.join(DIRECTORY, 'courses')
         MAIN_LOG = 'main.log'
@@ -20,7 +23,7 @@ class Canvas(object):
     TARGET_OUTCOME_ID = 2501  # ArcGIS Mapping Skills
     CONFIG_COURSE_ID = 138596
     CONFIG_COURSE_PAGE_NAME = 'course-ids'
-    COURSE_ID_SET = set((
+    COURSE_ID_SET = set(( # Used iff IDs are not found in the configuration course page defined above
         85489,  # Practice Course for Lance Sloan (LANCE PRACTICE)
         114488,  # First ArcGIS Course (ARCGIS-1)
         135885,  # Another ArcGIS Course (ARCGIS-2)
@@ -28,7 +31,7 @@ class Canvas(object):
 
 
 class ArcGIS(object):
-    ORG_NAME = 'devumich'
+    ORG_NAME = 'devumich' # For server URL (see below) and appended to ArcGIS usernames (i.e., "user_org")
     SECURITYINFO = {
         'security_type': 'Portal',  # Default: "Portal". "Required option" by bug in some ArcREST versions.
         'org_url': 'https://{}.maps.arcgis.com'.format(ORG_NAME),
