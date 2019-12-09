b14fcc7d9ac46fa96ee41cd8bb17fc0edcc1db9a
Lance E Sloan «UMich» <lsloan-github.com@umich.edu>
2019-12-09 15:28:44 -0500

    Lowercasing "kartograafr"

--- start.sh
+++ start.sh
@@ -2,16 +2,16 @@
 
 # This simple shell script performs a couple basic steps to set up the application to run in a container environment
 # (e.g. OpenShift). First it sets up a courses directory inside the mounted log volume (if necessary). Then it
-# determines whether Kartograafr will send logs by email or not; if log emails are wanted, SEND_EMAIL should be set
+# determines whether kartograafr will send logs by email or not; if log emails are wanted, SEND_EMAIL should be set
 # using the Deployment Config (.yaml).
 
 mkdir -p /var/log/kartograafr/courses
 
 if [ "${SEND_EMAIL}" == "True" ]; then
-  echo "Running Kartograafr WITH email flag"
+  echo "Running kartograafr WITH email flag"
   python main.py --email
 else
-  echo "Running Kartograafr WITHOUT email flag"
+  echo "Running kartograafr WITHOUT email flag"
   python main.py
 fi
 
